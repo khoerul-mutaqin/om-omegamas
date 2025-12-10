9da79057fe6872ba5bee1554c703c9ccfb62f3ef
khoerul-mutaqin <khoerul.mutaqin@falinwa.com>
2025-12-10 16:12:51 +0700

    [upd] add compute funct compute_value

--- cdp_om_bukubesar_piutang/models/stock_valuation_layer.py
+++ cdp_om_bukubesar_piutang/models/stock_valuation_layer.py
@@ -4,13 +4,31 @@ class StockValuationLayer(models.Model):
     _inherit = 'stock.valuation.layer'
     
 
-    x_in_valuation = fields.Monetary(string='In Valuation', currency_field="currency_id") 
-    x_out_valuation = fields.Monetary(string='Out Valuation', currency_field="currency_id")      
+    x_in_valuation = fields.Monetary(string='In Valuation', currency_field="currency_id", compute="compute_value")  
+    x_out_valuation = fields.Monetary(string='Out Valuation', currency_field="currency_id", compute="compute_value")     
     x_qty_balance = fields.Float(related='quantity', string='Balance')          
 
-    x_qty_in  = fields.Float(string='Qty In')          
-    x_qty_out = fields.Float(string='Qty Out')        
-    x_qty_start = fields.Float(string='Start')                 
+    x_qty_in  = fields.Float(string='Qty In', compute="compute_value")           
+    x_qty_out = fields.Float(string='Qty Out', compute="compute_value")    
+        
+    @api.depends('quantity','value')
+    def compute_value(self):
+        for rec in self:
+            balance =  rec.quantity
+            value = rec.value
+            if balance > 0:
+                rec['x_qty_in'] = balance
+                rec['x_in_valuation'] = value
+                rec['x_qty_out'] = 0
+                rec['x_out_valuation'] = 0
+            else:
+                rec['x_qty_in'] = 0
+                rec['x_in_valuation'] = 0
+                rec['x_qty_out'] = value
+                rec['x_out_valuation'] = balance
+                
+
+    x_qty_start = fields.Float(string='Start')        
     x_start_valuation = fields.Monetary(string='Start Valuation', currency_field="currency_id")          
     x_studio_date = fields.Datetime(related="product_id.stock_move_ids.move_line_ids.date", string="Order Date", readonly=False)
 
